da16be045a750e98e54d4e291af87458e97b8033
Tyler Veness <calcmogul@gmail.com>
2017-06-03 09:30:55 -0700

    Added experimental button tracking interface
    Change-Id: Iab19aa47ccb3520b02d3f5229ee9b70451ea2ae4

---
+++ src/XboxController.h
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2016-2017. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "ErrorBase.h"
+#include "GenericHID.h"
+
+namespace frc {
+
+/**
+ * Handle input from Xbox 360 or Xbox One controllers connected to the Driver
+ * Station.
+ *
+ * This class handles Xbox input that comes from the Driver Station. Each time a
+ * value is requested the most recent value is returend. There is a single class
+ * instance for each controller and the mapping of ports to hardware buttons
+ * depends on the code in the Driver Station.
+ */
+class XboxController : public GenericHID {
+ public:
+  enum ButtonType {
+    kBumper = 100,  // Larger than possible buttons
+    kStick = 101,   // Larger than possible buttons
+    kBumperLeft = 5,
+    kBumperRight = 6,
+    kStickLeft = 9,
+    kStickRight = 10,
+    kA = 1,
+    kB = 2,
+    kX = 3,
+    kY = 4,
+    kBack = 7,
+    kStart = 8
+  };
+
+  explicit XboxController(int port);
+  virtual ~XboxController() = default;
+
+  XboxController(const XboxController&) = delete;
+  XboxController& operator=(const XboxController&) = delete;
+
+  double GetX(JoystickHand hand) const override;
+  double GetY(JoystickHand hand) const override;
+
+  virtual double GetTriggerAxis(JoystickHand hand) const;
+
+  bool GetButton(ButtonType button, JoystickHand hand = kRightHand) const;
+  bool ButtonPressed(ButtonType button, JoystickHand hand = kRightHand);
+  bool ButtonReleased(ButtonType button, JoystickHand hand = kRightHand);
+
+ private:
+  uint32_t m_pressedButtons = 0;
+  uint32_t m_releasedButtons = 0;
+
+  int GetButtonIndex(ButtonType button, JoystickHand hand = kRightHand) const;
+};
+
+}  // namespace frc
